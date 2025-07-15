d0a5c80ee6a5195eab6fd3e8bdcd6736bbbe5f10
Shraphyn <baubetjacques@yahoo.com>
2025-07-16 01:51:41 +0200

    Added dic for characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import chara_times
 
 def get_book_text(path_file):
     with open(path_file) as f:
@@ -8,6 +9,8 @@ def get_book_text(path_file):
 def main():
     book_content = get_book_text("books/frankenstein.txt")
     wrd = count_words(book_content)
+    chara = chara_times(book_content)
     word_count = f"{wrd} words found in the document"
     print (word_count)
+    print(chara)
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def count_words(book_content):
     return len(book_content.split())
+
+def chara_times(text):
+    n = text.lower()
+    dic = {}
+    for txt in n: 
+        if txt in dic:
+            dic[txt] += 1
+        else:
+            dic[txt] = 1
+    return dic
